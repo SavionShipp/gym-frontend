ba025c2ced6c382ed52f4c708ab56e720c6fa4fa
SavionShipp <shippsavion209@yahoo.com>
2024-11-10 10:28:58 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { GymPage } from "./GymPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <GymPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
