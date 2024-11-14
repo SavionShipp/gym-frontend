e4ee3d7dd48863d3a57e463258bb75b8a33b73c6
SavionShipp <shippsavion209@yahoo.com>
2024-11-13 16:14:04 -0800

    changed show page

--- src/GymShowPage.jsx
+++ src/GymShowPage.jsx
@@ -1,42 +1,51 @@
 import { useLoaderData } from "react-router-dom";
-import { useState} from "react";
 import axios from 'axios';
-import  {GymNew}  from "./GymNew";
+import { GymNew } from "./GymNew";
+
 export function GymShowPage() {
-  const [exercise, setExercise] = useState([]);
+  const routine = useLoaderData();
 
-  const handleCreate = (params, successCallback) => {
-         console.log("handleCreate", params);
-         axios.post("http://localhost:3000/exercise.json", params).then((response) => {
-           setExercise([...exercise, response.data]);
-           successCallback();
-         });
-       };
+  const handleCreate = (exercise,params, successCallback) => {
+    console.log("handleCreate", params);
+    axios.post(`http://localhost:3000/exercise/${exercise.id}.json`, params)
+      .then((response) => {
+        console.log(response.data); 
+        successCallback();
+      })
+      .catch((error) => {
+        console.error("Error creating exercise:", error); 
+      });
+  };
 
-  const user = useLoaderData();
-
-  console.log(user);
+  console.log(routine);
 
   return (
     <div>
-      <h1>Name:{user.name}</h1> 
-      <hr />
-      <p>Email:{user.email}</p>
-      <hr />
-      <img src={user.image_url} />
-      {/* <form>
-         <div>
-           Name: <input defaultValue={user.name} name="name" type="text" />
-         </div>
-         <div>
-           email: <input defaultValue={user.email} name="email" type="text" />
-         </div>
-         <div>
-           image: <input defaultValue={user.image_url} name="image" type="text" />
-         </div>
-         <button type="submit">Update</button> */}
-       {/* </form> */}
+      <h1>User</h1>
+      {routine.users.map(user => (
+        <div key={user.id}>
+          <h1>Name: {user.name}</h1>
+          <p>Email: {user.email}</p>
+          <hr />
+          <img src={user.image_url} alt="profile picture" />
+        </div> 
+      ))}
+  
+      <h1>Exercise</h1>
+      {routine.exercises.map(exercise => (
+        <div key={exercise.id}>
+          <h1>{exercise.name}</h1>
+          <p>{exercise.description}</p>
+          <img src={exercise.image_url} alt="exercise" />
+          <p>Reps: {routine.reps}</p>
+          <p>Sets: {routine.sets}</p>
+        </div> 
+      ))}
       <GymNew onCreate={handleCreate} />
-      </div>
+    </div> 
   );
+  
+
+
+
 }
